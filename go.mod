90002b265a3d5d24f1db7fc52fdba187602e78b6
Alex Dovenmuehle <alex.dovenmuehle@mattermost.com>
2020-03-20 15:19:10 -0400

    Able to call and pretty print JSON of favorite looks and dashboards.

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/adovenmuehle/mattermost-looker-plugin
 go 1.12
 
 require (
+	github.com/adovenmuehle/mattermost-looker-plugin/openapi v0.0.0-20200320170818-8c1c7bdc1283
+	github.com/antihax/optional v1.0.0
 	github.com/mattermost/mattermost-server/v5 v5.20.0
 	github.com/mholt/archiver/v3 v3.3.0
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.5.1
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 )
--- main.go
+++ main.go
@@ -2,10 +2,45 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/antihax/optional"
+	"golang.org/x/oauth2"
 
 	api "github.com/adovenmuehle/mattermost-looker-plugin/openapi"
 )
 
 func main() {
-	auth := context.WithValue(context.Background(), api.ContextAPIKey, api.APIKey{Key: "TEST123"})
+	clientID := os.Getenv("LOOKER_API_KEY")
+	clientSecret := os.Getenv("LOOKER_API_SECRET")
+	client := api.NewAPIClient(api.NewConfiguration())
+
+	accessToken, _, _ := client.ApiAuthApi.Login(context.Background(), &api.LoginOpts{ClientId: optional.NewString(clientID), ClientSecret: optional.NewString(clientSecret)})
+
+	token := oauth2.Token{
+		AccessToken: accessToken.AccessToken,
+		Expiry:      time.Now().Add(time.Duration(accessToken.ExpiresIn)),
+		TokenType:   accessToken.TokenType,
+	}
+
+	tokenSource := oauth2.StaticTokenSource(&token)
+
+	auth := context.WithValue(context.Background(), api.ContextOAuth2, tokenSource)
+	userID := optional.NewInt64(int64(47))
+
+	r, _, _ := client.ContentApi.SearchContentFavorites(auth, &api.SearchContentFavoritesOpts{UserId: userID})
+
+	prettyJSON, err := json.MarshalIndent(r, "", "    ")
+	if err != nil {
+		log.Fatal("Failed to generate json", err)
+	}
+	fmt.Printf("%s\n", string(prettyJSON))
+
+	// r, _, _ := client.LookApi.RunLook(auth, int64(85), "md", &api.RunLookOpts{})
+
+	// fmt.Println(r)
 }
